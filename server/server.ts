b2e228d6d22e2aa6bc7bf04b9739e5d0e8745996
Otso Saarinen <saarinenotso@gmail.com>
2025-10-18 23:06:41 +0300

    fix bug with page routing

--- server/server.ts
+++ server/server.ts
@@ -3,31 +3,16 @@ import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
 
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
-
-const swaggerOptions = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Föli API",
-			version: "1.0.0",
-			description: "API wrapper for Föli transportation data",
-		},
-		servers: [
-			{
-				url: `http://localhost:${port}`,
-			},
-			{ url: "https://data.foli.fi/siri" },
-		],
-	},
-	apis: ["./server/*.ts"],
-};
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/sm", async (req, res) => {
 	const response = await fetch("https://data.foli.fi/siri/sm");
@@ -71,6 +56,34 @@ app.get("/vm/:id", async (req, res) => {
 	} else {
 		res.status(404).json({ error: "Data not found" });
 	}
+});
+
+const swaggerOptions = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Föli API",
+			version: "1.0.0",
+			description: "API wrapper for Föli transportation data",
+		},
+		servers: [
+			{
+				url: `http://localhost:${port}`,
+			},
+			{ url: "https://data.foli.fi/siri" },
+		],
+	},
+	apis: ["./server/*.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use(express.static(join(__dirname, "../dist")));
+
+app.use((req, res) => {
+	res.sendFile(join(__dirname, "../dist", "index.html"));
 });
 
 app.listen(port, () => {
