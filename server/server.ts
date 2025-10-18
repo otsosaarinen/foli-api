512f7b8f383e787247f0be480b2b009f28d306f5
Otso Saarinen <saarinenotso@gmail.com>
2025-10-18 19:08:40 +0300

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -27,7 +27,6 @@ const swaggerOptions = {
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/sm", async (req, res) => {
