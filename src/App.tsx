0df4cdb071f9b68a14dbc7b597c691bc2951aeea
Otso Saarinen <saarinenotso@gmail.com>
2025-10-18 22:50:46 +0300

    update frontend info text

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,27 @@ function App() {
 		<>
 			<div className="flex h-screen flex-col items-center justify-start gap-10 bg-neutral-800 text-yellow-400">
 				<h1 className="mt-20 text-8xl font-bold">FÖLI API</h1>
-				<h2 className="text-md w-200 text-center font-medium">
+				<h2 className="font-medium text-white">
+					Read the API documentation{" "}
+					<a href="" className="underline">
+						here
+					</a>
+				</h2>
+				<h3 className="w-200 text-center font-medium">
 					All data on this website originates from the public
 					transport operations and timetable data of the Turku region.
 					The dataset is maintained by the Public Transport Office of
 					the City of Turku. The data has been downloaded from{" "}
-					<a href="http://data.foli.fi/" className="underline">
+					<a
+						href="http://data.foli.fi/"
+						target="_blank"
+						className="underline"
+					>
 						http://data.foli.fi/
 					</a>{" "}
 					under the Creative Commons Attribution 4.0 International (CC
 					BY 4.0) license.
-				</h2>
+				</h3>
 			</div>
 		</>
 	);
