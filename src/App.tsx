f5d13f619430f7b97af26532e51968f3e9c5e0ac
Otso Saarinen <saarinenotso@gmail.com>
2025-10-19 11:59:26 +0300

    add api documentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,71 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-	const [name, setName] = useState("unknown");
-
 	return (
 		<>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button
-					onClick={() => setCount((count) => count + 1)}
-					aria-label="increment"
-				>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
+			<div className="flex h-screen flex-col items-center justify-start gap-10 bg-neutral-800 text-yellow-300">
+				<h1 className="mt-20 text-9xl font-bold">FÖLI API</h1>
+
+				<h2 className="text-white">
+					<div className="text-xl font-medium">Endpoints</div>
+					<div className="my-2 h-0.5 w-200 bg-yellow-300"></div>
+					<div className="flex flex-col gap-1 text-lg font-medium">
+						<div>
+							<div>
+								{" "}
+								<span className="rounded-lg bg-green-600 px-3 py-0.5">
+									GET
+								</span>{" "}
+								/api/sm
+							</div>
+							<div className="text-base italic">
+								Returns a list of Föli bus stops
+							</div>
+						</div>
+						<div>
+							<div>
+								{" "}
+								<span className="rounded-lg bg-green-600 px-3 py-0.5">
+									GET
+								</span>{" "}
+								/api/sm/:id
+							</div>
+							<div className="text-base italic">
+								Returns details of the specified bus stop, like
+								the arriving buses and their coordinates
+							</div>
+						</div>
+						<div>
+							<div>
+								{" "}
+								<span className="rounded-lg bg-green-600 px-3 py-0.5">
+									GET
+								</span>{" "}
+								/api/vm
+							</div>
+							<div className="text-base italic">
+								Returns a list of all Föli buses and their
+								detailed information
+							</div>
+						</div>
+					</div>
+				</h2>
+				<h3 className="w-200 text-center font-medium">
+					API datasource: Public transport operation and timetable
+					data of the Turku region. The dataset is maintained by the
+					City of Turku Public Transport Office. The data was
+					downloaded from{" "}
+					<a
+						href="http://data.foli.fi/"
+						target="_blank"
+						className="underline"
+					>
+						http://data.foli.fi/
+					</a>{" "}
+					under the Creative Commons Attribution 4.0 International (CC
+					BY 4.0) license.
+				</h3>
 			</div>
-			<div className="card">
-				<button
-					onClick={() => {
-						fetch("/api/")
-							.then(
-								(res) =>
-									res.json() as Promise<{ name: string }>,
-							)
-							.then((data) => setName(data.name));
-					}}
-					aria-label="get name"
-				>
-					Name from API is: {name}
-				</button>
-				<p>
-					Edit <code>api/index.ts</code> to change the name
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	);
 }
